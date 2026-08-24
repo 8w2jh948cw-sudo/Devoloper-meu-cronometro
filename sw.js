9622ffce437fb331c6482299a0f3a18084c4bcee
8w2jh948cw-sudo <8w2jh948cw@privaterelay.appleid.com>
2026-08-24 18:54:30 -0300

    Update service worker to Cronômetro v0.8.0

--- sw.js
+++ sw.js
@@ -1,16 +1,33 @@
-const APP_VERSION = "0.5.2";
-const CACHE = 'cronometro-unified-v050-dev0-navfloat-051';
-const ASSETS = ['./','./index.html','./styles.css','./app.js','./design-config.js','./jszip.min.js','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(Promise.all([
-  caches.keys().then(keys => Promise.all(keys.filter(k => k.startsWith('cronometro-') && k !== CACHE).map(k => caches.delete(k)))),
-  self.clients.claim()
-])));
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(fetch(e.request).then(res => {
-    const copy = res.clone();
-    caches.open(CACHE).then(c => c.put(e.request, copy));
-    return res;
-  }).catch(() => caches.match(e.request).then(hit => hit || caches.match('./index.html'))));
+'use strict';
+const CACHE='cronometro-0.8.0';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./initial-data.json'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
+    caches.open(CACHE)
+      .then(cache=>cache.addAll(ASSETS))
+      .then(()=>self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    Promise.all([
+      caches.keys().then(keys=>Promise.all(
+        keys.filter(key=>key.startsWith('cronometro-')&&key!==CACHE).map(key=>caches.delete(key))
+      )),
+      self.clients.claim()
+    ])
+  );
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(
+    fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./index.html')))
+  );
 });
