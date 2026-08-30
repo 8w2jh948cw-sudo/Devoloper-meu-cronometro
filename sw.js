9a9c5979480539330fe27f42738d299782215e79
8w2jh948cw-sudo <8w2jh948cw@privaterelay.appleid.com>
2026-08-30 03:22:57 -0300

    Update service worker cache to v0.8.3

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 'use strict';
-const CACHE='cronometro-0.8.2';
+const CACHE='cronometro-0.8.3';
 const ASSETS=[
   './','./index.html','./manifest.webmanifest','./icon.svg','./initial-data.json',
   './cronometro-v080-01.css','./cronometro-v080-02.css','./cronometro-v080-03.css',
@@ -9,7 +9,8 @@ const ASSETS=[
   './cronometro-v081-overrides.css','./cronometro-v081-version.js',
   './cronometro-v081-overrides-1.js','./cronometro-v081-overrides-2.js','./cronometro-v081-overrides-3.js',
   './cronometro-v082-overrides.css','./cronometro-v082-01.js','./cronometro-v082-02.js',
-  './cronometro-v082-03.js','./cronometro-v082-04.js','./cronometro-v082-05.js'
+  './cronometro-v082-03.js','./cronometro-v082-04.js','./cronometro-v082-05.js',
+  './cronometro-v083-fixes.css','./cronometro-v083-version.js'
 ];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',event=>{event.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('cronometro-')&&key!==CACHE).map(key=>caches.delete(key)))),self.clients.claim()]));});
